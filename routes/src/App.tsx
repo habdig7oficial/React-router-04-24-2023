12b93d1dbf1e39d333c571fde306b5d8b0170ff0
Mateus Vieira <habdig7@gmail.com>
2023-04-24 13:25:39 -0300

    Router add

--- routes/src/App.tsx
+++ routes/src/App.tsx
@@ -1,12 +1,19 @@
 import Home from "./components/Home";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sobre from "./components/Sobre";
+import Contato from "./components/Contatos";
+
 export default function App() {
   return(
     <>
-      <h1>Hello</h1>
-
-      <Home />
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={ <Home /> }></Route>
+          <Route path="/sobre"  element={ <Sobre /> }></Route>
+          <Route path="/contato"  element={ <Contato /> }></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- routes/src/components/Menu.tsx
+++ routes/src/components/Menu.tsx
@@ -3,8 +3,8 @@ export default function Menu() {
     return(
       <>
         <a href="/">Home Page</a>   	&nbsp;	&nbsp;	&nbsp;
-        <a href="/">Contatos</a>	    &nbsp;  &nbsp;  &nbsp;
-        <a href="/">Sobre</a> <br />	&nbsp;	&nbsp;	&nbsp;
+        <a href="/contato">Contatos</a>	    &nbsp;  &nbsp;  &nbsp;
+        <a href="/sobre">Sobre</a> <br />	&nbsp;	&nbsp;	&nbsp;
       </>
     )
   }
